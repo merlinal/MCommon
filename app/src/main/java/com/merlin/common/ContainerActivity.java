539e028b50c2102a0e3b387f7097cb0f08f987bf
ncm <anlin@newchama.comn>
2017-05-27 11:14:46 +0800

    fix

---
+++ app/src/main/java/com/merlin/common/ContainerActivity.java
@@ -0,0 +1,69 @@
+package com.merlin.common;
+
+import android.content.Intent;
+import android.databinding.DataBindingUtil;
+import android.os.Bundle;
+
+import com.merlin.common.databinding.ContainerBinding;
+import com.merlin.core.at.MustFragment;
+import com.merlin.core.base.AbstractActivity;
+import com.merlin.core.base.AbstractFragment;
+import com.merlin.core.base.AbstractVM;
+import com.merlin.core.util.Util;
+import com.merlin.view.bar.model.Bar;
+
+/**
+ * Created by ncm on 2017/5/27.
+ */
+
+public class ContainerActivity extends AbstractActivity<AbstractVM, ContainerBinding> {
+
+    public static void start(AbstractFragment srcFragment, @MustFragment Class<?> cls, Bundle bundle, int requestCode) {
+        Intent it = new Intent(srcFragment.getContext(), ContainerActivity.class);
+        it.putExtra("fragmentName", cls.getName());
+        it.putExtras(bundle);
+        srcFragment.startActivityForResult(it, requestCode);
+    }
+
+    private AbstractFragment fragment;
+    private Bar bar;
+
+    @Override
+    public void initData() {
+        super.initData();
+        bar = new Bar.Builder().build();
+    }
+
+    @Override
+    public void handleParam() {
+        super.handleParam();
+        Intent it = getIntent();
+        fragment = Util.loadClass(it.getStringExtra("fragmentName"));
+    }
+
+    @Override
+    protected int getLayoutResId() {
+        binding = DataBindingUtil.setContentView(this, R.layout.container);
+        binding.setBarModel(bar);
+        return 0;
+    }
+
+    @Override
+    public void initView() {
+        super.initView();
+        getSupportFragmentManager()
+                .beginTransaction()
+                .replace(R.id.container_page, fragment)
+                .commitAllowingStateLoss();
+    }
+
+    @Override
+    public void onBackPressed() {
+        fragment.onBackPressed();
+    }
+
+    protected Bar bar() {
+        return bar;
+    }
+
+}
